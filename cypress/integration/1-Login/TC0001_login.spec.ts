cab87889c56d86e975e1a3c6eeabe7b9f1968a48
Iftekhar Mishu <iftekhar.mishu@gmail.com>
2022-02-05 14:37:25 +0000

    typos

--- cypress/integration/1-Login/TC0001_login.spec.ts
+++ cypress/integration/1-Login/TC0001_login.spec.ts
@@ -4,13 +4,13 @@ import { LoginPage } from "../../pages/login_page"
 import { PostLogInPage } from "../../pages/postLogIn_page"
 
 const loginPage = new LoginPage()
-const postLoginPage = new PostLogInPage()
+const postLogInPage = new PostLogInPage()
 
 it('Valid Login and Logout Test', function(){
     loginPage.navigateToLoginPage()
     loginPage.enterLoginCredentials('Admin', 'admin123')
     loginPage.clickLogin()
-    postLoginPage.validateProfileGreeting()
-    postLoginPage.logout()
+    postLogInPage.validateProfileGreeting()
+    postLogInPage.logout()
     loginPage.validateRedirectedLoginUrl()
 })
