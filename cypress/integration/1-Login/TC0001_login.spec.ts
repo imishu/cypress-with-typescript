cd788738cebec64b8150dd00708d65d063cd8d67
CtrlShiftR Ltd <iftekhar.mishu@gmail.com>
2022-02-09 20:45:31 +0000

    Add before hook (#7)

--- cypress/integration/1-Login/TC0001_login.spec.ts
+++ cypress/integration/1-Login/TC0001_login.spec.ts
@@ -6,8 +6,11 @@ import { PostLogInPage } from "../../pages/postLogIn_page"
 const logInPage = new LogInPage()
 const postLogInPage = new PostLogInPage()
 
-it('Valid Login and Logout Test', function(){
+before(function(){
     logInPage.navigateToLoginPage()
+})
+
+it('Valid Login and Logout Test', function(){
     logInPage.enterLoginCredentials('Admin', 'admin123')
     logInPage.clickLogin()
     postLogInPage.validateProfileGreeting()
